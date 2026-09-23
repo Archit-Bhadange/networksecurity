3d95d9cf73f4af98c9121d34ffb3ab7e59219b2b
Archit-Bhadange <archit.bhadange@gmail.com>
2026-09-23 20:56:21 +0530

    setup.py done

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """returns list of requirements"""
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r')as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                ##ignore  -e. and empty lines
+                if requirement and requirement !='-e.':
+                    requirement_lst.append(requirement)
+
+    except FileNotFoundError:
+        print("requirement.txt file not found")
+
+    return requirement_lst
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Archit",
+    author_email="archit.bhadange@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
